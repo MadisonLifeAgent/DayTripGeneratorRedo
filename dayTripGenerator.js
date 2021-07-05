7edea30a9114e5621d10383d2c9ba23bb2bac9e6
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-05 14:19:20 -0500

    reduced trip details selection functions from four down to one and added another function for user confirmation of trip details

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -9,89 +9,35 @@ let possibleRestaurants = ["McDonalds", "Culvers", "Burger King", "Taco Bell", "
 let possibleEntertainment = ["Baseball Game", "Theatrical Play", "Live Music", "Festival / Flea Market", "Parks and Trails", "Museums", "Circus"];
 
 // Get the randomly selected trip details
-let randomDestination = getDestination(possibleDestinations);
-let randomRestaurant = getRestaurant(possibleRestaurants);
-let randomTransportMode = getTransportMode(possibleTransportModes);
-let randomEntertainment = getEntertainment(possibleEntertainment);
+let randomDestination = getTripItem(possibleDestinations);
+let randomRestaurant = getTripItem(possibleRestaurants);
+let randomTransportMode = getTripItem(possibleTransportModes);
+let randomEntertainment = getTripItem(possibleEntertainment);
 
-// select a destination for user
-function getDestination(destinationArray){
+// select a trip item for user
+function getTripItem(tripItemArray){
 
-    // generate random destination selection
-    let chosenDestination = destinationArray[Math.floor(Math.random() * destinationArray.length)];
+    // generate random trip item
+    let chosenTripItem = tripItemArray[Math.floor(Math.random() * tripItemArray.length)];
 
-    // ask if user likes destination
-    let destinationChoice = prompt("Would you like to go to " + chosenDestination + "?", "yes or no");
+    // ask if user likes trip item generated
+    let tripItemChoice = prompt("Would you like to go to " + chosenTripItem + "?", "yes or no");
 
-    // if user doesn't like destination, rerun selection until user does
-    while (destinationChoice !== "yes"){
-        chosenDestination = destinationArray[Math.floor(Math.random() * destinationArray.length)];
-        destinationChoice = prompt("Would you like this mode of transportation: " + chosenDestination + "?", "yes or no");
+    // if user doesn't like trip item, rerun selection until user does
+    while (tripItemChoice !== "yes"){
+        chosenTripItem = tripItemArray[Math.floor(Math.random() * tripItemArray.length)];
+        tripItemChoice = prompt("Would you like this mode of transportation: " + chosenTripItem + "?", "yes or no");
     }
 
     // send selection back to where called
-    return chosenDestination;
+    return chosenTripItem;
 }
-
-// select a restaurant for user
-function getRestaurant(restaurantArray){
-
-    // generate random restaurant selection
-    let chosenRestaurant = restaurantArray[Math.floor(Math.random() * restaurantArray.length)];
-
-    // ask if user likes restaurant
-    let restaurantChoice = prompt("Would you like to eat at " + chosenRestaurant + "?", "yes or no");
-
-    // if user doesn't like restaurant, rerun selection until they do
-    while (restaurantChoice !== "yes"){
-        chosenRestaurant = restaurantArray[Math.floor(Math.random() * restaurantArray.length)];
-        restaurantChoice = prompt("Would you like to eat at " + chosenRestaurant + "?", "yes or no");
-    }
-
-    // send selection back to where called
-    return chosenRestaurant;
-}
-
-// select a transportation mode for user
-function getTransportMode(transportModeArray){
-
-    // generate random transportation mode selection
-    let chosenTransportMode = transportModeArray[Math.floor(Math.random() * transportModeArray.length)];
-
-    // ask if user likes transportation mode
-    let transportChoice = prompt("Would you like to go by " + chosenTransportMode + "?", "yes or no");
-
-    // if user doesn't like selected transportation mode, rerun selection until they do
-    while (transportChoice !== "yes"){
-        chosenTransportMode = transportModeArray[Math.floor(Math.random() * transportModeArray.length)];
-        transportChoice = prompt("Would you like to go by " + chosenTransportMode + "?", "yes or no");
-    }
-
-    // send selection back to where called
-    return chosenTransportMode;
-}
-
-// select a form of entertainment for user
-function getEntertainment(entertainmentArray){
-
-    // generate random entertainment selection
-    let chosenEntertainment = entertainmentArray[Math.floor(Math.random() * entertainmentArray.length)];
-
-    // ask if user likes restaurant
-    let entertainmentChoice = prompt("Would you like this form of entertainment, " + chosenEntertainment + "?", "yes or no");
-
-    // if user doesn't like transportation mode, rerun selection until they do
-    while (entertainmentChoice !== "yes"){
-        chosenEntertainment = entertainmentArray[Math.floor(Math.random() * entertainmentArray.length)];
-        entertainmentChoice = prompt("Would you like this form of entertainment, " + chosenEntertainment + "?", "yes or no");
-    }
-
-    // send selection back to where called
-    return chosenEntertainment;
-}
-
 
 // confirm if user likes trip details they selected
+let tripDetailConfirmation = userConfirmationPrompt();
+
+// get user confirmation
+function userConfirmationPrompt(){
     let userConfirmation = prompt("Here are your trip details." + 
                                 "\n" + 
                                 "Type 'yes' to complete your trip or 'no' to get another trip. " +
@@ -103,15 +49,22 @@ function getEntertainment(entertainmentArray){
                                 "Restaurant: " + randomRestaurant +
                                 "\n" +
                                 "Entertainment: " + randomEntertainment, "yes or no");
+        // console.log(userConfirmation);
+        return userConfirmation;
+}
 
+// verify user confirmation
+let userFinalAnswer = verifyUserConfirmation(tripDetailConfirmation)
 
+// confirm if user likes trip details they selected
+function verifyUserConfirmation(userConfirmation){
     while (userConfirmation !== "yes"){
 
         // Regenerate the trip selections again
-        randomDestination = getDestination(possibleDestinations);
-        randomTransportMode = getTransportMode(possibleTransportModes);
-        randomRestaurant = getRestaurant(possibleRestaurants);
-        randomEntertainment = getEntertainment(possibleEntertainment);
+        randomDestination = getTripItem(possibleDestinations);
+        randomRestaurant = getTripItem(possibleRestaurants);
+        randomTransportMode = getTripItem(possibleTransportModes);
+        randomEntertainment = getTripItem(possibleEntertainment);
 
         // display newly generated trip details in prompt and ask user to confirm
         userConfirmation = prompt("Here are your trip details. Type 'yes' to confirm or 'no' to start over." +
@@ -124,8 +77,9 @@ function getEntertainment(entertainmentArray){
                                 "\n" +
                                 "Entertainment: " + randomEntertainment, "yes or no");
     }
+
     
-    // after user confirms trip, redisplay trip details and wish them well on their trip
+    // if user confirms trip, redisplay trip details and wish them well on their trip
     if (userConfirmation === "yes"){
         alert("Your trip is complete, enjoy!" +
                 "\n\n" +
@@ -137,6 +91,8 @@ function getEntertainment(entertainmentArray){
                 "\n" +
                 "Entertainment: " + randomEntertainment);
     }
+}
+
 
     // display user confirmed trip details one last time
     console.log("Trip Details" +
